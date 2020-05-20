1dc01aac0cccbc0bb4c09722f4bdad635b789513
olaidegbeyide <toungbeyide9@gmail.com>
2020-05-20 02:40:33 +0100

    new codes added

--- tutorial.js
+++ tutorial.js
@@ -4,11 +4,15 @@ const loadDetails = async () => {
  await fetch('https://jsonplaceholder.typicode.com/comments')
  .then(response => response.json())
  .then(data => {
+    console.log(data);
      data.map(deets =>{
+         
+         
          html += `
                 
-         <p class="name">${deets.name}</p>
+         <p class="name">${deets.name }</p>
          <p><b>${deets.email}</b></p>
+         <p class="name"><b> Email length: </b>${deets.email.length}</p>
          <p class="name">${deets.body}</p>
    `
      })
@@ -17,3 +21,6 @@ const loadDetails = async () => {
 }
 loadDetails()
 
+
+
+   
