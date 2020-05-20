6c950f4b797a4f24fa3ce4e6ce1be559a4f8c4f1
olaidegbeyide <toungbeyide9@gmail.com>
2020-05-20 01:56:30 +0100

    First commit

---
+++ tutorial.js
@@ -0,0 +1,18 @@
+const showDeets = document.querySelector('.container')
+const loadDetails = async () => {
+    let html =``
+ await fetch('https://jsonplaceholder.typicode.com/comments')
+ .then(response => response.json())
+ .then(data => {
+     data.map(deets =>{
+         html += `
+                
+         <p class="name">${deets.name}</p>
+         <p><b>${deets.email}</b></p>
+         <p class="name">${deets.body}</p>
+   `
+     })
+     showDeets.innerHTML = html
+ })
+}
+loadData()
